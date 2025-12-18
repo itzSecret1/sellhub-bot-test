73206603d5b6b12ef1d8cfb3acc2567b548c59ec
itzSecret01 <adamelouinissi@gmail.com>
2025-12-18 16:22:34 +0100

    Fix API base URL to use correct SellHub endpoint structure

--- classes/Api.js
+++ classes/Api.js
@@ -3,7 +3,7 @@ import { config } from '../utils/config.js';
 
 export class Api {
   constructor() {
-    this.baseUrl = 'https://dash.sellhub.cx/api/';
+    this.baseUrl = 'https://dash.sellhub.cx/api/sellhub/';
     this.apiKey = config.SH_API_KEY;
     this.shopId = config.SH_SHOP_ID;
   }
